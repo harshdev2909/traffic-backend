a5ee5cade8b8b1091ca9d76114592f01f6c2d2e4
harshdev2909 <harsh.dev2909@gmail.com>
2024-10-18 13:31:45 +0530

    added

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
 	try {
 		console.log("mongo_uri: ", process.env.MONGO_URI);
-		const conn = await mongoose.connect(process.env.MONGO_URI);
+		const conn = await mongoose.connect("mongodb+srv://harshdev2909:harsh9560@cluster0.ew8dn.mongodb.net/");
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.log("Error connection to MongoDB: ", error.message);
